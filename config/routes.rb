f154fbb2cd4b139c7bf9ce4dce2c5d5787c14fea
Akane Saito <ukku19@gmail.com>
2023-09-11 13:01:45 +0200

    now edit links work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Read all
+  get '/tasks', to: 'tasks#index'
+  # Create
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  # Read one
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # Update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  # Delete
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  # or all above can be replaced by just one following line
+  # resources :tasks
 end
